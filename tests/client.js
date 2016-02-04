4dd314125bf2160d6c531c3669daba3801c0409d
Ryan Glover <me@ryanglover.net>
2016-02-03 22:42:44 -0600

    wip refactoring tests to remove jQuery

--- tests/client.js
+++ tests/client.js
@@ -2,133 +2,138 @@ Tinytest.add( 'Bert is available on the client', function ( test ) {
   test.equal( typeof Bert, 'object' );
 });
 
-Tinytest.add( 'Bert can set an alert on Session', function ( test ) {
+Tinytest.addAsync( 'Bert can set an alert on Session', function ( test, next ) {
   var testMessage = 'Testing Bert\'s ability to display an alert.';
 
   Bert.alert( testMessage );
   var message = Session.get( 'bertAlert' ).message;
 
-  test.equal( message, testMessage );
+  Meteor.setTimeout( function() {
+    test.equal( message, testMessage );
+    next();
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a default message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'default' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'default' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a success message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'success' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'success' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'success' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set an info message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'info' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'info' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'info' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a warning message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'warning' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'warning' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'warning' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a danger message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'danger' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'danger' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'danger' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a fixed-top message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'fixed-top' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'fixed-top' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'fixed-top' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a fixed-bottom message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'fixed-bottom' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'fixed-bottom' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'fixed-bottom' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a growl-top-left message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'growl-top-left' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'growl-top-left' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'growl-top-left' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a growl-top-right message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'growl-top-right' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'growl-top-right' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'growl-top-right' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a growl-bottom-left message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'growl-bottom-left' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'growl-bottom-left' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'growl-bottom-left' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a growl-bottom-right message', function ( test, next ) {
   Bert.alert( 'Testing 123', 'default', 'growl-bottom-right' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'growl-bottom-right' );
+    var hasClass = document.getElementById( 'bert-alert' ).classList.contains( 'growl-bottom-right' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a custom icon', function ( test, next ) {
-  Bert.alert( 'Testing 123', 'default', 'fixed-top', 'fa-amazon' );
+  Bert.alert( 'Testing 123', 'default', 'fixed-top', 'fa fa-amazon' );
+
+  var alertElement = document.getElementById( 'bert-alert' );
 
   Meteor.setTimeout( function() {
-    var hasClass = document.getElementsById( 'bert-alert' ).classList.contains( 'fa-amazon' );
+    var hasClass = alertElement.getElementsByTagName( 'i' )[ 0 ].classList.contains( 'fa-amazon' );
     test.equal( hasClass, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
 
 Tinytest.addAsync( 'Bert can set a complex message', function ( test, next ) {
@@ -137,16 +142,19 @@ Tinytest.addAsync( 'Bert can set a complex message', function ( test, next ) {
     message: 'Artist &mdash; Song Name',
     type: 'info',
     style: 'growl-top-right',
-    icon: 'fa-music'
+    icon: 'fa fa-music'
   });
 
   Meteor.setTimeout( function() {
+    var alertElement = document.getElementById( 'bert-alert' ),
+        alertClasses = alertElement.classList;
+
     var alert = {
-      title: $( '.bert-alert .bert-content h5' ).text(),
-      message: $( '.bert-alert .bert-content p' ).text(),
-      type: document.getElementsById( 'bert-alert' ).classList.contains( 'info' ),
-      style: document.getElementsById( 'bert-alert' ).classList.contains( 'growl-top-right' ),
-      icon: document.getElementsById( 'bert-alert' ).classList.contains( 'fa-music' )
+      title: alertElement.getElementsByTagName( 'h5' )[ 0 ].innerText,
+      message: alertElement.getElementsByTagName( 'p' )[ 0 ].innerText,
+      type: alertClasses.contains( 'info' ),
+      style: alertClasses.contains( 'growl-top-right' ),
+      icon: alertElement.getElementsByTagName( 'i' )[ 0 ].classList.contains( 'fa-music' )
     };
 
     test.equal( alert.title, 'Now Playing' );
@@ -155,5 +163,5 @@ Tinytest.addAsync( 'Bert can set a complex message', function ( test, next ) {
     test.equal( alert.style, true );
     test.equal( alert.icon, true );
     next();
-  }, 500 );
+  }, 1000 );
 });
