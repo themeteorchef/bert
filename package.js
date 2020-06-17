031dc5c5aaaf2c5447bc4de67521c615acba91f5
Ryan Glover <business@cleverbeagle.com>
2020-06-17 18:52:33 -0500

    bump jquery version

--- package.js
+++ package.js
@@ -1,37 +1,43 @@
 Package.describe({
-  name: 'themeteorchef:bert',
-  version: '2.2.0',
-  summary: 'A client side, multi-style alerts system for Meteor.',
-  git: 'http://github.com/themeteorchef/bert',
-  documentation: 'README.md'
+  name: "themeteorchef:bert",
+  version: "2.2.0",
+  summary: "A client side, multi-style alerts system for Meteor.",
+  git: "http://github.com/themeteorchef/bert",
+  documentation: "README.md",
 });
 
-Package.onUse( function( api ) {
-  api.versionsFrom( '1.2.0.2' );
+Package.onUse(function (api) {
+  api.versionsFrom("1.2.0.2");
 
-  api.use([
-    'ecmascript',
-    'templating',
-    'session',
-    'jquery',
-    'fourseven:scss@4.12.0',
-  ], 'client');
+  api.use(
+    [
+      "ecmascript",
+      "templating",
+      "session",
+      "jquery@3.0.0",
+      "fourseven:scss@4.12.0",
+    ],
+    "client"
+  );
 
-  api.addFiles([
-    'templates/bert-alert.html',
-    'templates/bert-alert.js',
-    'templates/body.html',
-    'stylesheets/colors.scss',
-    'stylesheets/flexbox.scss',
-    'stylesheets/bert.scss',
-    'bert.js',
-  ], 'client');
+  api.addFiles(
+    [
+      "templates/bert-alert.html",
+      "templates/bert-alert.js",
+      "templates/body.html",
+      "stylesheets/colors.scss",
+      "stylesheets/flexbox.scss",
+      "stylesheets/bert.scss",
+      "bert.js",
+    ],
+    "client"
+  );
 
-  api.export( 'Bert' );
+  api.export("Bert");
 });
 
 Package.onTest(function (api) {
-  api.use( [ 'tinytest', 'session', 'jquery' ] );
-  api.use( 'themeteorchef:bert' );
-  api.addFiles( 'tests/client.js', 'client' );
+  api.use(["tinytest", "session", "jquery"]);
+  api.use("themeteorchef:bert");
+  api.addFiles("tests/client.js", "client");
 });
